28bb14f73386e0acc4749505a2d68e2509d6bb05
cbandag <cbandag@unicartagena.edu.co>
2023-09-12 13:06:56 -0500

    carlos merge sixto

--- app/Http/Controllers/FacultadesController.php
+++ app/Http/Controllers/FacultadesController.php
@@ -27,7 +27,6 @@ class FacultadesController extends Controller
     public function create()
     {
         return view('facultades.create');
-        return view('facultades.create');
     }
 
     /**
